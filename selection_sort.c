807ee34af4949dd308c9ac5ae5fbe27b27622c67
Oussama Sallak <osallak@student.1337.ma>
2022-03-04 21:35:40 +0100

    Selection sort updated

--- selection_sort.c
+++ selection_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   selection_sort.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: osallak <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: osallak <osallak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 10:54:09 by osallak           #+#    #+#             */
-/*   Updated: 2022/03/04 11:09:04 by osallak          ###   ########.fr       */
+/*   Updated: 2022/03/04 21:35:29 by osallak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int	main(void)
 	int tab[] = {5, 4, 55, 0, -1};
 	selection_sort(tab, 5);
 	//it will look like this after we call the sort function : -1 0 4 5 55
+	//print the result
 	for (int i = 0; i < 5; i++)
 		printf("%d ", tab[i]);
 }
